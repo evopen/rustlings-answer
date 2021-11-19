d95462662d01204238a34285dd4f2200b8b35621
evopen <520dhh@gmail.com>
2021-11-19 16:58:24 +0800

    4.6.0 modified excercises

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -30,8 +30,9 @@ impl Package {
         self.sender_country != self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
         // Something goes here...
+        self.weight_in_grams * cents_per_gram
     }
 }
 
