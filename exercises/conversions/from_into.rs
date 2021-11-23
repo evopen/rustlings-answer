df0f38cedcea6864914f56e3b89b2ee3d40eee8e
evopen <520dhh@gmail.com>
2021-11-23 09:48:37 +0800

    complete conversions exercises

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,27 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.is_empty() {
+            return Person::default();
+        }
+        if let Some(ss) = s.split_once(',') {
+            let name = ss.0;
+            if name.is_empty() {
+                return Default::default();
+            }
+            if let Ok(age) = ss.1.parse::<usize>() {
+                return Person {
+                    name: name.to_string(),
+                    age,
+                };
+            } else {
+                return Person::default();
+            }
+        } else {
+            return Person::default();
+        }
     }
 }
 
--- exercises/conversions/from_str.rs
+++ exercises/conversions/from_str.rs
@@ -26,8 +26,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -41,6 +39,26 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.is_empty() {
+            return Err(ParsePersonError::Empty);
+        }
+        let ss = s.split(',').collect::<Vec<_>>();
+        if ss.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+        let name = ss[0];
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+        match ss[1].parse::<usize>() {
+            Ok(age) => {
+                return Ok(Person {
+                    name: name.to_string(),
+                    age,
+                })
+            }
+            Err(e) => return Err(ParsePersonError::ParseInt(e)),
+        }
     }
 }
 
